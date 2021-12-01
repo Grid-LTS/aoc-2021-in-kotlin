7af90479f8c56ed1ccd38149a8d8824530d5f6f1
Gerd Friemel <gerd.friemel@tngtech.com>
2021-12-01 23:11:30 +0100

    day01,2021: sonar sweep

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increases = 0;
+        for (i in 1..input.size-1) {
+            if (input[i].toInt() > input[i-1].toInt()){
+                increases++
+            }
+        }
+        return increases
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun partSlidingWindow(input: List<String>): Int {
+        var increases = 0;
+        var sumA = 0
+        var start = 1
+        for (j in -1..1) {
+            sumA += input[start+j].toInt()
+        }
+        start++
+        var sumB = 0
+        for (i in start..input.size-2) {
+            for (j in -1..1) {
+                sumB += input[i + j].toInt()
+            }
+            if (sumB > sumA){
+                increases++
+            }
+            sumA = sumB
+            sumB = 0
+        }
+        return increases
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // test sample
+    val testInput = readInput("test_resources/Day01_test")
+    check(part1(testInput) == 7)
+    check(partSlidingWindow(testInput) == 5)
 
-    val input = readInput("Day01")
+    var input = readInput("resources/Day01_sonar")
     println(part1(input))
-    println(part2(input))
+    println(partSlidingWindow(input))
 }
